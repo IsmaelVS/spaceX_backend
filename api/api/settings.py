394c27bd4adb3a5da8362d8c48331d774cf2c4ec
Ismael Ventura <ismael.venturadasilva0@gmail.com>
2019-11-17 14:37:12 -0300

    Correção no settings.

--- api/api/settings.py
+++ api/api/settings.py
@@ -120,5 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_TMP = os.path.join(BASE_DIR, 'static')
-os.makedirs(STATIC_TMP, exist_ok=True)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
