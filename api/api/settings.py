4200c882981469fb6a1e391237bc7c39338fb30f
Ismael Ventura <ismael.venturadasilva0@gmail.com>
2019-11-17 12:04:10 -0300

    Adicionando nova configuração para o heroku no settings.

--- api/api/settings.py
+++ api/api/settings.py
@@ -120,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_TMP = os.path.join(BASE_DIR, 'static')
+os.makedirs(STATIC_TMP, exist_ok=True)
